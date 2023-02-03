4a71fadb05f45a032517446f966d054f8ca3abb7
FEstrada-0987 <florencia.estrada@hotmail.com>
2023-02-03 00:22:39 -0300

    Funciones para pintar tareas del modal al html

--- checkList.js
+++ checkList.js
@@ -1,29 +1,46 @@
                 //Formulario + Boton crear
 
+const tareas =[]
+
+function $(elementByHtml){
+    return document.querySelector(elementByHtml)
+}
+
 window.addEventListener('load', function() {
+            //--> Formulario <---
+    let $form = document.querySelector(".form");
+    let $formErrors = document.querySelector("#formError")
 
-    let form = document.querySelector(".form");
-    let formErrors = document.querySelector("#formError")
+            //--> Input Titulos <---
+    let $title = document.querySelector("#title")  
+    let $titleErrors = document.querySelector("#title-error")
 
-    let title = document.querySelector("#title")  
-    let titleErrors = document.querySelector("#title-error")
+           //--> Select Estados <---
+    let $state = document.querySelector("#state")
+    let $stateErrors = document.querySelector("#state-errors")
 
-    let state = document.querySelector("#state")
-    let stateError = document.querySelector("#state-errors")
-    
+          //--> Abre y cierra modal <---
     const openModal = document.querySelector("#open-modal")
     const containModal = document.querySelector(".container-modal")
     const closeModal = document.querySelector("#close")
+
+         //--> Abre y cierra modal <---
     const trashButton = document.querySelector("#trash")
     const editButton = document.querySelector("#edit")
-
-    const toDo = document.getElementById("toDo")
-
+   
+         //--> Modo oscuro <---
     const btnSwitch = document.querySelector('#switch');
+    
+        //--> Array de tareas
+    // const tareas = []
 
-    const dateText = document.getElementById('dateTxt');
-
-   ///Modo Oscuro
+    // const dateText = document.getElementById('dateTxt');
+    // const $regExpAlpha = /^[a-zA-Z0-9-\sñáéíóúüª!:?'¡].{5,20}$/
+         // --> Constantes de columnas html
+         const TareaToDo = document.querySelector("#toDo")
+         const TareainProgress = document.querySelector("#inProgress")
+         const Tareacomplete = document.querySelector("#complete")
+        //-->Modo Oscuro
     btnSwitch.addEventListener('click', () => {
         document.getElementById('darki').classList.toggle('dark');
         document.getElementById('header').classList.toggle('dark');
@@ -54,66 +71,57 @@ window.addEventListener('load', function() {
 
     })
    
-    form.addEventListener("submit", (e) => {
+    $form.addEventListener("submit", (e) => {
 
         let errors = false
         e.preventDefault()
+       
 
-        if(title.value.length <+5){
-            titleErrors.innerHTML = "Necesito un titulo mejor :)"
+        if($title.value.length <+5){
+            $titleErrors.innerHTML = "Necesito un titulo mejor :)"
             errors = true
         }else{
-            stateError.innerHTML = ""
+            $titleErrors.innerHTML = ""
         }
 
         if(!errors){
             console.log("submit paso correctamente!")
+        } 
+        
+        if($state.value.length = " "){
+            $stateErrors.innerHTML = "Seleccione una opcion para su tarea"
+            errors = true
+        }else{
+            $stateErrors.innertext = ""
         }
-    })
 
-///FECHA del dia en NAV
-    const setDate = () => {
-        const date = new Date();  
-        dateText.textContent = date.toLocaleString('es', {day: 'numeric',weekday: 'long',month: 'short', year: 'numeric'});                                                        
-    };
-    setDate();
+        if(!errors){
+            console.log("submit paso correctamente!")
+        } 
+        
+        // paint($containTareas, tareas)
 
-    
-
-
-    // const createTask = document.getElementById('create-button');
-    // const toDoTask = document.getElementById('toDo');
-    
-    // createTask.addEventListener('click', (event) =>{
-    //     const pulsando = event.target.value;
-    //     toDoTask.innerHTML = pulsando;
-    // })
-
-
-// const taskContainer = document.getElementById('divTask')
-
-    //  const addNewTask = event => {
-    //  event.preventDefault();
-    //      const { value } = event.target.taskText;
-    //      if(!value) return;
-
-    //     // const task = document.createElement('div');
-    //      task.classList.add('task', roundBorder);
-    //      task.addEventListener('click')
-    //      task.textContent = value;
-    //      taskContainer.prepend(task);
-    //      event.target.reset();
-    //  };
-
-    // const changeTask= event => {
-    //     event.target.classList.toggle('done');
-    // }
-
-    // const order = () => {
-    //     const done = [];
-    //     const toDo = [];
-    //     tasksContainer.childNodes.forEach( e =>{
-    //                 e.classList.contains('done') ? done.push(e) : toDo.push(e)
-    // })
-    //}
+        //  function paint (nodo,datos){
+        //     nodo.innerHTML = "";
+        //     datos.forEach(tarea => {
+        //         nodo.innerHTML +=
+        $title.addEventListener('input', (event) => {
+            const textoIngresado = event.target.value;
+            TareaToDo.innerHTML = textoIngresado;
+        })
+        $state.addEventListener('select', (event) => {
+            const selectIngresado = event.target.value;
+            TareaToDo.innerHTML = selectIngresado;
+        })
+        // TareaToDo.innerHTML +=
+        //         `<div class="">
+        //         <p>${$title} </p>
+        //         <p>${$state} </p>
+        //         <button> EDIT</button>
+        //         <button> DELETE</button>
+        //     </div>`                
+        //     });
+        // }
+        
+     })
 })
