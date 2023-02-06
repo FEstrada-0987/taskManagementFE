3d49436d89ce325f38484d0dd0b7045a98615540
FEstrada-0987 <florencia.estrada@hotmail.com>
2023-02-05 23:59:43 -0300

    probando nuevas funciones para pintar en pantalla

--- checkList.js
+++ checkList.js
@@ -13,7 +13,6 @@
      btnSwitch.addEventListener('click', () => {
          document.getElementById('darki').classList.toggle('dark');
          document.getElementById('nav').classList.toggle('dark'); 
-         document.getElementById('header').classList.toggle('dark');
          document.getElementById('tableFormat').classList.toggle('dark');
 
         btnSwitch.classList.toggle('active');  
@@ -32,30 +31,50 @@
     const form = document.getElementById('formModal');
     const taskName = document.getElementById('task-Name');
     const selectState = document.getElementById('select-state');
-    const description = document.getElementById('description');
+    const descriptionT = document.getElementById('description');
 
 //----> MI section para mostrar las tareas
 
-    const sectionAllTask = document.getElementById('tableFormat');
+    const sectionAllTask = document.getElementById('transactionTable');
 
  //-----> BOTONES
  
     const buttonCreateModal = document.querySelector('#createTask');
-
+//---> input buscar tarea
+    const searchTask = document.querySelector('#search-Task');
 
    let allTask = []
             
     form.addEventListener('submit', function (e) {
     //      console.log(e)
-    //     e.preventDefault();
+        e.preventDefault();
+        
+        const crearTarea = (title,state,description) =>{
+
+          allTask.push({
+            title:taskName.value,
+            state:selectState.value,
+            description: descriptionT.value,
+          })
+                  return allTask
+        } 
+               console.log(crearTarea("hola", "si", "probando"))
+               console.log(allTask)
+               sectionAllTask.innerHTML =""
+    })
+
+
+
     //     let transactionFormData = new FormData(form);
     //     let transactionTableRef = document.getElementById("transactionTable");
     //     let newTransactionRow = transactionTableRef.insertRow(-1);
     //     let newTypeCellRef = newTransactionRow.insertCell(0);
     //     newTypeCellRef.textContent = "dfdsfdsf"
     // })
-    buttonCreateModal.addEventListener('click', (e) => {
-      e.preventDefault(); //nuevo objeto para cada usuario
+   
+
+
+     // e.preventDefault(); //nuevo objeto para cada usuario
     //   const addTasks = {
     //     id: uuidv4(),
     //     nameTask: taskName.value,
@@ -70,23 +89,27 @@
     //   paint()
     // })
 
-    const paint = (nodo, array) => {
-      nodo.innerHTML = ""
-      array.forEach(element => {
-          nodo.innerHTML += `<div>
-              <p>${element.nameTask.value}</p>
-              <p>${element.state.value}</p>
-              <p>${element.description.value}</p>
-              <button class="btn-delete" id=${element.id}>Eliminar</button>
-              <button class="btn-edit" id=${element.id}>Editar</button>
-          </div>`
-      });
-    }
+    // const paint = (nodo, array) => {
+    //   nodo.innerHTML = ""
+    //   array.forEach(element => {
+    //       nodo.innerHTML += `<div>
+    //           <p>${element.nameTask.value}</p>
+    //           <p>${element.state.value}</p>
+    //           <p>${element.description.value}</p>
+    //           <button class="button is-primary" id=${element.id}>Eliminar</button>
+    //           <button class="button is-warning delete" id=${element.id}>Editar</button>
+    //       </div>`
+    //   });
+    // }
     
-  })
-  paint(sectionAllTask, allTask)
 
-  })
+
+
+
+
+  //paint(sectionAllTask,allTask)
+
+  //})
  
 
 
